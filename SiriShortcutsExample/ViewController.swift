859f9621e5cd8b6234a91b4d6e6b45b9195a7b20
Sinan Ege <mehmetsinanege@gmail.com>
2018-10-03 00:02:57 +0300

    Initial Commit

---
+++ SiriShortcutsExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SiriShortcutsExample
+//
+//  Created by Sinan Ege on 3.10.2018.
+//  Copyright © 2018 Sinan Ege. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
