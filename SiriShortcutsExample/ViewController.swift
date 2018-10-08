13a888f248485c9e80520a291037d3ad224f5e86
Sinan Ege <>
2018-10-08 22:56:26 +0300

    Implementation of Siri shortcuts via NSUserActivity

--- SiriShortcutsExample/ViewController.swift
+++ SiriShortcutsExample/ViewController.swift
@@ -7,14 +7,85 @@
 //
 
 import UIKit
+import Intents
 
-class ViewController: UIViewController {
+enum DrinkType: String {
+    case water
+    case coffee
+    case tea
+}
 
+internal class ViewController: UIViewController {
+    
+    let currentDefaults = UserDefaults.standard
+    
+    // MARK: IBOutlets
+    @IBOutlet private weak var buttonWater: UIButton!
+    @IBOutlet private weak var buttonCoffee: UIButton!
+    @IBOutlet private weak var buttonTea: UIButton!
+    
+    // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        buttonWater.setTitle("Glass of Water (\(currentCount(for: .water)))", for: .normal)
+        buttonCoffee.setTitle("Cup of Coffee (\(currentCount(for: .coffee)))", for: .normal)
+        buttonTea.setTitle("Cup of Tea (\(currentCount(for: .tea)))", for: .normal)
     }
-
+    
+    @IBAction private func glassOfWaterButtonTapped(_ sender: Any?) {
+        self.increment(for: .water)
+        buttonWater.setTitle("Glass of Water (\(currentCount(for: .water)))", for: .normal)
+        self.donateUserActivity(with: .water)
+    }
+    
+    @IBAction private func cupOfCoffeeButtonTapped(_ sender: Any?) {
+        increment(for: .coffee)
+        buttonCoffee.setTitle("Cup of Coffee (\(currentCount(for: .coffee)))", for: .normal)
+        self.donateUserActivity(with: .coffee)
+    }
+    
+    @IBAction private func cupOfTeaButtonTapped(_ sender: Any?) {
+        increment(for: .tea)
+        buttonTea.setTitle("Cup of Tea (\(currentCount(for: .tea)))", for: .normal)
+        self.donateUserActivity(with: .tea)
+    }
+    
+    // MARK: Private Utilities
+    private func increment(for drink: DrinkType) {
+        let updatedValue = self.currentCount(for: drink) + 1
+        currentDefaults.set(updatedValue, forKey: drink.rawValue)
+    }
+    
+    private func currentCount(for drink: DrinkType) -> Int {
+        return currentDefaults.integer(forKey: drink.rawValue)
+    }
+    
+    // MARK: Public Action
+    public func didDrank(_ drink: DrinkType) {
+        switch drink {
+        case .water:
+            self.glassOfWaterButtonTapped(.none)
+        case .coffee:
+            self.cupOfCoffeeButtonTapped(.none)
+        case .tea:
+            self.cupOfTeaButtonTapped(.none)
+        }
+    }
 
 }
 
+// MARK: Donate User Activity due to DrinkType
+extension ViewController {
+    func donateUserActivity(with type: DrinkType) {
+        let activityTypeName = "com.sinanege.SiriExample.drinkCount"
+        let activity = NSUserActivity(activityType: activityTypeName)
+        activity.title = "I just drank \(type.rawValue)"
+        activity.userInfo = ["drinkType" : type.rawValue]
+        activity.isEligibleForSearch = true
+        activity.isEligibleForPrediction = true
+        activity.suggestedInvocationPhrase = "Drank \(type.rawValue)"
+        view.userActivity = activity
+        activity.becomeCurrent()
+    }
+}
